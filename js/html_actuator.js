c73f5b419eee86fa1018bea7376381aa2e4b9a17
phase <ph4sesaber@gmail.com>
2015-11-01 21:20:08 -0800

    update share score tweet

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -155,13 +155,13 @@ HTMLActuator.prototype.scoreTweetButton = function () {
   var tweet = document.createElement("a");
   tweet.classList.add("twitter-share-button");
   tweet.setAttribute("href", "https://twitter.com/share");
-  tweet.setAttribute("data-via", "gabrielecirulli");
-  tweet.setAttribute("data-url", "http://git.io/2048");
-  tweet.setAttribute("data-counturl", "http://gabrielecirulli.github.io/2048/");
+  tweet.setAttribute("data-via", "Phasesaber");
+  tweet.setAttribute("data-url", "http://jadonfowler.xyz/2048");
+  tweet.setAttribute("data-counturl", "http://jadonfowler.xyz/2048/");
   tweet.textContent = "Tweet";
 
-  var text = "I scored " + this.score + " points at 2048, a game where you " +
-             "join numbers to score high! #2048game";
+  var text = "I scored " + this.score + " points at 1073741824, a game where you " +
+             "join numbers to score high! #1073741824game";
   tweet.setAttribute("data-text", text);
 
   return tweet;
